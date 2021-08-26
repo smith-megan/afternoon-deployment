fc048e744d131a1ab318b0ecfb0cf5ca070c748f
Megan <smith.megan.design@gmail.com>
2021-08-26 14:06:40 -0600

    sending

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ app.get("/", (req, res)=>{
   res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
-app.get('/css', (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.css"))
-})
+// app.get('/css', (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/index.css"))
+// })
+
+app.use('/css', express.static(path.join(__dirname, 'client/index.css')))
 
 const PORT=process.env.PORT || 2200
 
