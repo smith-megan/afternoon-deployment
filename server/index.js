500cc8f5fa77c98dc3437ec26831c6f29b6550eb
Megan <smith.megan.design@gmail.com>
2021-08-26 13:28:14 -0600

    finished step check

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express= require('express')
 const path = require('path')
 
-app=express()
+app=(express())
 
 app.get("/", (req, res)=>{
-  res.sendFile(path.join(__dirname, "..client/index.html"))
+  res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
-const PORT=process.env.PORT || 2000
+const PORT=process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
-`server is up and running on ${PORT}`})
+app.listen(PORT, ()=>{console.log(`server is up and running on ${PORT}`)})
