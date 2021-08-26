c4ffac0cc153358f84b69b776722bfb9dedd8f1e
Megan <smith.megan.design@gmail.com>
2021-08-26 13:19:30 -0600

    files ready again

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 app=express()
 
 app.get("/", (req, res)=>{
-  res.sendFile(path.join(__dirname, "../index.html"))
+  res.sendFile(path.join(__dirname, "..client/index.html"))
 })
 
 const PORT=process.env.PORT || 2000
