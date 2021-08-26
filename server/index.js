d0802f41a131c59e5a5fe7a82355ded9061ca0bc
Megan <smith.megan.design@gmail.com>
2021-08-26 13:46:17 -0600

    ready

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res)=>{
   res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
+app.get('/css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.css'))
+})
+
 const PORT=process.env.PORT || 3000
 
 app.listen(PORT, ()=>{console.log(`server is up and running on ${PORT}`)})
