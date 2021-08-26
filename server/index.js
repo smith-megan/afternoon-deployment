03ccef83c53856bc3bffa1152d2f6ba28d642824
Megan <smith.megan.design@gmail.com>
2021-08-26 13:29:18 -0600

    finished

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const path = require('path')
 
-app=(express())
+const app=(express())
 
 app.get("/", (req, res)=>{
   res.sendFile(path.join(__dirname, "../client/index.html"))
